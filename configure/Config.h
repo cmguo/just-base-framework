703ef8a13d667f49d5b19a9770c79ca8faa6f35e
rainleafchen <rainleafchen@pptv.com>
2011-08-18 07:29:37 +0000

    修改配置逻辑，满足ipad对配置信息保存等要求
    git-svn-id: http://svn01/repositories/r01/base/branches/1.2.1@585 b3a1389e-2159-5c43-9038-463bf877d0d9

--- configure/Config.h
+++ configure/Config.h
@@ -15,6 +15,7 @@
 #include "framework/string/Parse.h"
 #include "framework/system/LogicError.h"
 
+
 //#include <boost/function.hpp>
 
 namespace framework
@@ -26,11 +27,10 @@ namespace framework
         class ConfigItem
         {
         public:
-            // 访问许可标志定义
-            enum Flag {
-                allow_set = 1, 
-                allow_get = 2, 
-            };
+            // 配置参数初始化函数类型
+            typedef void (*initor_type)(
+                ConfigItem &, 
+                std::string const &);
 
             // 配置参数设置器函数类型
             typedef boost::system::error_code (*settor_type)(
@@ -57,10 +57,12 @@ namespace framework
 
             ConfigItem(
                 unsigned int flag, 
+                initor_type initor, 
                 settor_type settor, 
                 gettor_type gettor, 
                 deletor_type deletor)
                 : flag_(flag)
+                , initor_(initor)
                 , settor_(settor)
                 , gettor_(gettor)
                 , deletor_(deletor)
@@ -76,19 +78,21 @@ namespace framework
                 ConfigItem const & r);
 
         public:
+            void init(
+                std::string const & str)
+            {
+                initor_(*this, str);
+            }
+
             boost::system::error_code set(
                 std::string const & str)
             {
-                if (!(flag_ & allow_set))
-                    return framework::system::logic_error::no_permission;
                 return settor_(*this, str);
             }
 
             boost::system::error_code get(
                 std::string & str) const
             {
-                if (!(flag_ & allow_get))
-                    return framework::system::logic_error::no_permission;
                 return gettor_(*this, str);
             }
 
@@ -97,20 +101,9 @@ namespace framework
                 deletor_(*this);
             }
 
-            unsigned int flag() const
-            {
-                return flag_;
-            }
+            unsigned int flag_;
 
-        private:
-            friend class Config;
-            boost::system::error_code init(
-                std::string const & str)
-            {
-                return settor_(*this, str);
-            }
-
-            unsigned int flag_;  // 访问许可标志
+            initor_type initor_; // 初始化器
             settor_type settor_; // 设置器
             gettor_type gettor_; // 读取器
             deletor_type deletor_; // 销毁器
@@ -130,8 +123,9 @@ namespace framework
 
             ~ConfigModule()
             {
-                for (const_iterator ik = begin(); ik != end(); ++ik) {
+                for (iterator ik = begin(); ik != end(); ++ik) {
                     (ik->second)->del();
+                    (ik->second) = NULL;
                 }
             }
 
@@ -202,31 +196,43 @@ namespace framework
         };
 
         // 默认的配置器
-        template <typename T>
-        struct ConfigItemT
+        template <typename T, typename C>
+        struct ConfigItemHelper
             : public ConfigItem
         {
-        public:
-            ConfigItemT(
-                T & t, 
-                unsigned int flag)
+            ConfigItemHelper( unsigned int flag = 0)
                 : ConfigItem(
-                    flag, 
-                    ConfigItemT::set, 
-                    ConfigItemT::get, 
-                    ConfigItemT::del)
-                , t_(t)
+                    flag,
+                    C::init,
+                    C::set, 
+                    C::get, 
+                    C::del)
             {
             }
 
         private:
+            // 直接解析字符串并初始化参数值
+            static void init(
+                ConfigItem & item, 
+                std::string const & str)
+            {
+                C & this_item = static_cast<C &>(item);
+                T t;
+                if (!framework::string::parse2(str, t))
+                    this_item.init_value(t);
+            }
+
             // 直接解析字符串并修改参数值本身
             static boost::system::error_code set(
                 ConfigItem & item, 
                 std::string const & str)
             {
-                ConfigItemT & this_item = static_cast<ConfigItemT &>(item);
-                return framework::string::parse2(str, this_item.t_);
+                C & this_item = static_cast<C &>(item);
+                T t;
+                boost::system::error_code ec = framework::string::parse2(str, t);
+                if (!ec)
+                    ec = this_item.set_value(t);
+                return ec;
             }
 
             // 直接从参数值本身格式化结果字符串
@@ -234,15 +240,83 @@ namespace framework
                 ConfigItem const & item, 
                 std::string & str)
             {
-                ConfigItemT const & this_item = static_cast<ConfigItemT const &>(item);
-                return framework::string::format2(str, this_item.t_);
+                C const & this_item = static_cast<C const &>(item);
+                T t;
+                boost::system::error_code ec = this_item.get_value(t);
+                if (!ec)
+                    ec = framework::string::format2(str, t);
+                return ec;
             }
 
             static void del(
                 ConfigItem & item)
             {
-                ConfigItemT & this_item = static_cast<ConfigItemT &>(item);
+                C & this_item = static_cast<C &>(item);
                 delete &this_item;
+            }
+
+            // 不允许
+            void init_value(
+                T const & t)
+            {
+            }
+
+            // 不允许
+            boost::system::error_code set_value(
+                T const & t)
+            {
+                return framework::system::logic_error::no_permission;
+            }
+
+            // 不允许
+            boost::system::error_code get_value(
+                T & t) const
+            {
+                return framework::system::logic_error::no_permission;
+            }
+        };
+
+        // 访问许可标志定义
+        enum Flag {
+            allow_set = 1, 
+            allow_get = 2, 
+        };
+
+        template <typename T>
+        struct ConfigItemT
+            : public ConfigItemHelper<T, ConfigItemT< T > >
+        {
+        public:
+            ConfigItemT(
+                T & t, 
+                unsigned int flag)
+                : ConfigItemHelper<T, ConfigItemT>( flag )
+                , t_(t)
+            {
+            }
+
+        public:
+            // 直接修改参数值本身
+            void init_value(
+                T const & t)
+            {
+                t_ = t;
+            }
+
+            // 直接修改参数值本身
+            boost::system::error_code set_value(
+                T const & t)
+            {
+                t_ = t;
+                return boost::system::error_code();
+            }
+
+            // 直接从参数值本身格式化结果字符串
+            boost::system::error_code get_value(
+                T & t) const
+            {
+                t =  t_;
+                return boost::system::error_code();
             }
 
         private:
@@ -350,7 +424,12 @@ namespace framework
             std::string const & key, 
             ConfigItem * item)
         {
+            iterator it = find( key );
+            if ( it != end() )
+                (it->second)->del();
+
             (*this)[key] = item;
+
             conf_.register_param(name_, key, item);
             return *this;
         }
@@ -373,20 +452,20 @@ namespace framework
 #define CONFIG_PARAM_RDONLY(p) \
     std::make_pair( \
     #p, \
-    framework::configure::make_item(p, (framework::configure::ConfigItem::allow_get)) \
+    framework::configure::make_item(p, (framework::configure::allow_get)) \
     )
 
 #define CONFIG_PARAM_WRONLY(p) \
     std::make_pair( \
     #p, \
-    framework::configure::make_item(p, (framework::configure::ConfigItem::allow_set)) \
+    framework::configure::make_item(p, (framework::configure::allow_set)) \
     )
 
 #define CONFIG_PARAM_RDWR(p) \
     std::make_pair( \
     #p, \
     framework::configure::make_item(p, \
-    (framework::configure::ConfigItem::allow_get | framework::configure::ConfigItem::allow_set)) \
+    (framework::configure::allow_get | framework::configure::allow_set)) \
     )
 
 #define CONFIG_PARAM_NAME(name, p, flag) \
@@ -404,20 +483,21 @@ namespace framework
 #define CONFIG_PARAM_NAME_RDONLY(name, p) \
     std::make_pair( \
     name, \
-    framework::configure::make_item(p, (framework::configure::ConfigItem::allow_get)) \
+    framework::configure::make_item(p, (framework::configure::allow_get)) \
     )
 
 #define CONFIG_PARAM_NAME_WRONLY(name, p) \
     std::make_pair( \
     name, \
-    framework::configure::make_item(p, (framework::configure::ConfigItem::allow_set)) \
+    framework::configure::make_item(p, (framework::configure::allow_set)) \
     )
 
 #define CONFIG_PARAM_NAME_RDWR(name, p) \
     std::make_pair( \
     name, \
     framework::configure::make_item(p, \
-    (framework::configure::ConfigItem::allow_get | framework::configure::ConfigItem::allow_set)) \
+    (framework::configure::allow_get | framework::configure::allow_set)) \
     )
+
 
 #endif // __FRAMEWORK_CONFIGURE_CONFIG_H_
