8a3c2ac4fa75899fb62f8158319a3c6cb6b614e2
darrenhe <darrenhe@pptv.com>
2011-08-26 07:05:55 +0000

    1.2.1 添加gtest_framework测试头文件TestHeader
    git-svn-id: http://svn01/repositories/r01/base/branches/1.2.1@693 b3a1389e-2159-5c43-9038-463bf877d0d9

---
+++ TestHeader.h
@@ -0,0 +1,13 @@
+
+/*------------------------------------------------------*/
+/*************** LoggerÄ£¿é°×ºÐ²âÊÔcppÎÄ¼þ **************/
+/*------------------------------------------------------*/
+
+
+#include "framework/Framework.h"
+#include "framework/logger/LoggerManager.h"
+#include "framework/logger/Logger.h"
+#include "framework/logger/LoggerRecord.h"
+#include "framework/logger/LoggerStreamRecord.h"
+#include "framework/logger/LoggerFileStream.h"
+
