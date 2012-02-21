08e4c5c63fbc5b41436297afdefb09b5c8163937
isxxguo <isxxguo@pptv.com>
2012-02-21 01:48:13 +0000

    /base/branches/1.2.1 include/framework/system/ScaleTransform.h 增加精度转换类，可以防止乘法溢出
    git-svn-id: http://svn01/repositories/r01/base/branches/1.2.1@2596 b3a1389e-2159-5c43-9038-463bf877d0d9

---
+++ system/ScaleTransform.h
@@ -0,0 +1,173 @@
+// ScaleTransform.h
+
+#ifndef _FRAMEWORK_SYSTEM_SCALE_TRANSFORM_H_
+#define _FRAMEWORK_SYSTEM_SCALE_TRANSFORM_H_
+
+namespace framework
+{
+    namespace system
+    {
+
+        class ScaleTransform
+        {
+        public:
+            ScaleTransform(
+                boost::uint64_t scale_in, 
+                boost::uint64_t scale_out)
+                : last_in_(0)
+                , last_out_(0)
+                , last_left_(0)
+            {
+                boost::uint64_t cd = common_divisor(scale_in, scale_out);
+                scale_in_ = scale_in / cd;
+                scale_out_ = scale_out / cd;
+                Transform t = {0, 0};
+                trans1_.resize(256);
+                for (size_t i = 0; i < 256; ++i) {
+                    trans1_[i] = t;
+                    t.left += scale_out_;
+                    if (t.left >= scale_in_) {
+                        boost::uint64_t n = t.left / scale_in_;
+                        t.out += n;
+                        t.left -= n * scale_in_;
+                    }
+                }
+                trans2_.resize(56);
+                for (size_t i = 0; i < 56; ++i) {
+                    trans2_[i] = t;
+                    t.out <<= 1;
+                    t.left <<= 1;
+                    if (t.left >= scale_in_) {
+                        ++t.out;
+                        t.left -= scale_in_;
+                    }
+                }
+            }
+ 
+        public:
+            boost::uint64_t inc(
+                boost::uint64_t n)
+            {
+                if (scale_in_ == 1) {
+                    if (scale_out_ == 1) {
+                        return last_out_ += n;
+                    } else {
+                        last_out_ += n * scale_out_;
+                        return last_out_;
+                    }
+                }
+                last_out_ += trans1_[n & 0xff].out;
+                last_left_ += trans1_[n & 0xff].left;
+                if (last_left_ >= scale_in_) {
+                    ++last_out_;
+                    last_left_ -= scale_in_;
+                }
+                n >>= 8;
+                if (n) {
+                    size_t i = 0;
+                    do {
+                        if (n & 1) {
+                            last_out_ += trans2_[i].out;
+                            last_left_ += trans2_[i].left;
+                            if (last_left_ >= scale_in_) {
+                                ++last_out_;
+                                last_left_ -= scale_in_;
+                            }
+                        }
+                        n >>= 1;
+                        ++i;
+                    } while (n);
+                }
+                return last_out_;
+            }
+
+            boost::uint64_t dec(
+                boost::uint64_t n)
+            {
+                if (scale_in_ == 1) {
+                    if (scale_out_ == 1) {
+                        return last_out_ += n;
+                    } else {
+                        last_out_ -= n * scale_out_;
+                        return last_out_;
+                    }
+                }
+                last_out_ -= trans1_[n & 0xff].out;
+                if (last_left_ >= trans1_[n & 0xff].left) {
+                    last_left_ -= trans1_[n & 0xff].left;
+                } else {
+                    --last_out_;
+                    last_left_ += scale_out_ - trans1_[n & 0xff].left;
+                }
+                n >>= 8;
+                if (n) {
+                    size_t i = 0;
+                    do {
+                        if (n & 1) {
+                            last_out_ += trans2_[i].out;
+                            if (last_left_ >= trans2_[i].left) {
+                                last_left_ -= trans2_[i].left;
+                            } else {
+                                --last_out_;
+                                last_left_ += scale_out_ - trans2_[i].left;
+                            }
+                        }
+                        n >>= 1;
+                        ++i;
+                    } while (n);
+                }
+                return last_out_;
+            }
+
+            boost::uint64_t transfer(
+                boost::uint64_t n)
+            {
+                return (n > last_in_) ? inc(n - last_in_) : dec(last_in_ - n);
+            }
+
+            boost::uint64_t get() const
+            {
+                return last_out_;
+            }
+ 
+        private:
+            static boost::uint64_t common_divisor(
+                boost::uint64_t scale_in, 
+                boost::uint64_t scale_out)
+            {
+                while (scale_in != scale_out) {
+                    if (scale_in > scale_out) {
+                        scale_in -= scale_in / scale_out * scale_out;
+                        if (scale_in == 0)
+                            return scale_out;
+                    } else {
+                        scale_out -= scale_out / scale_in * scale_in;
+                        if (scale_out == 0)
+                            return scale_in;
+                    }
+                }
+                return scale_in;
+            }
+
+        private:
+            struct Transform
+            {
+                boost::uint64_t out;
+                boost::uint64_t left;
+            };
+
+            boost::uint64_t scale_in_;
+            boost::uint64_t scale_out_;
+            std::vector<Transform> trans1_;
+            std::vector<Transform> trans2_;
+
+            boost::uint64_t last_in_;
+            boost::uint64_t last_out_;
+            boost::uint64_t last_left_;
+        };
+
+    }
+}
+
+#endif // _FRAMEWORK_SYSTEM_SCALE_TRANSFORM_H_
+
