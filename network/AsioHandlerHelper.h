55cf35af862db6c94c966299aeed64db66626ab3
isxxguo <isxxguo@pptv.com>
2012-08-09 09:28:10 +0000

    /base/branches/1.2.1 /include/framework/network/AsioHandlerHelper.h 增加asio_handler_*的直接传递实现宏
    git-svn-id: http://svn01/repositories/r01/base/branches/1.2.1@4468 b3a1389e-2159-5c43-9038-463bf877d0d9

---
+++ network/AsioHandlerHelper.h
@@ -0,0 +1,34 @@
+// AsioHandlerHelper.h
+
+#ifndef _FRAMEWORK_NETWORK_ASIO_HANDLER_HELPER_H_
+#define _FRAMEWORK_NETWORK_ASIO_HANDLER_HELPER_H_
+
+#define PASS_DOWN_ASIO_HANDLER_FUNCTION(type, member) \
+ \
+inline friend void* asio_memberallocate( \
+    std::size_t size, \
+    type * this_handler) \
+{ \
+    return boost_asio_memberalloc_helpers::allocate( \
+        size, &this_handler->member); \
+} \
+ \
+inline friend void asio_memberdeallocate( \
+    void * pointer, \
+    std::size_t size, \
+    type * this_handler) \
+{ \
+    boost_asio_memberalloc_helpers::deallocate( \
+        pointer, size, &this_handler->member); \
+} \
+ \
+template <typename Function> \
+inline friend void asio_memberinvoke( \
+                                       const Function & function, \
+                                       type * this_handler) \
+{ \
+    boost_asio_memberinvoke_helpers::invoke( \
+        function, &this_handler->member); \
+} \
+
+#endif // _FRAMEWORK_NETWORK_ASIO_HANDLER_HELPER_H_
