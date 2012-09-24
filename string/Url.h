aa4544de9594eb761e2a1331b03e35d4fe79c550
isxxguo <isxxguo@pptv.com>
2012-09-24 10:07:41 +0000

    git-svn-id: http://svn01/repositories/r01/base/branches/1.2.2@4982 b3a1389e-2159-5c43-9038-463bf877d0d9

--- string/Url.h
+++ string/Url.h
@@ -179,6 +179,9 @@ namespace framework
                 path_ = v;
             }
 
+            bool param_clear(
+                std::string const & key);
+
             void param(
                 std::string const & k, 
                 std::string const & v)
