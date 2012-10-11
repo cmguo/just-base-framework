cee534080d3422b90881b714b2d6785e2f7ee531
isxxguo <isxxguo@pptv.com>
2012-10-11 08:03:45 +0000

    /base/branches/1.2.2 /src/framework/logger/FormatRecord.cpp 适应 %1% 这样的形式
    git-svn-id: http://svn01/repositories/r01/base/branches/1.2.2@5168 b3a1389e-2159-5c43-9038-463bf877d0d9

---
+++ logger/FormatRecord.cpp
@@ -0,0 +1,38 @@
+// FormatRecord.cpp
+
+#include "framework/Framework.h"
+#include "framework/logger/FormatRecord.h"
+
+namespace framework
+{
+    namespace logger
+    {
+
+        static char const chex[] = "0123456789ABCDEF";
+
+        void FormatFormator::next_format()
+        {
+            assert(fmt_);
+            for (char const * p = fmt_; *p; ++p) {
+                if (*p == '%') {
+                    char const * q = p + 1;
+                    bool all_digit = true;
+                    while ((*q < 'A' || *q > 'Z') && (*q < 'a' || *q > 'z') && *q != '%') {
+                        all_digit = all_digit && (*q >='0' && *q <= '9');
+                        ++q;
+                    }
+                    os_.write(fmt_, p - fmt_);
+                    if (*q == '%' && !all_digit) { // 适应 %xx% 和 %N% 这样的形式，通过 all_digit 区分
+                        os_.put('%');
+                        p = q;
+                        fmt_ = q + 1;
+                    } else {
+                        fmt_ = q + 1;
+                        break;
+                    }
+                }
+            }
+        }
+
+    } // namespace logger
+} // namespace framework
