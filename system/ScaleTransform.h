d40d6d3704afe6ac9fbe46dc4c1c1717b31e5d2e
isxxguo <isxxguo@pptv.com>
2013-04-08 07:52:33 +0000

    /base/branches/1.2.2 /include/framework/system/ScaleTransform.h 增加支持精度转换中的平移需求（不经过原点） emailto: p2p, box-dev
    git-svn-id: http://svn01/repositories/r01/base/branches/1.2.2@6565 b3a1389e-2159-5c43-9038-463bf877d0d9

--- system/ScaleTransform.h
+++ system/ScaleTransform.h
@@ -29,24 +29,27 @@ namespace framework
                 , last_left_(0)
             {
             }
- 
+
             ScaleTransform(
                 boost::uint64_t scale_in, 
-                boost::uint64_t scale_out)
+                boost::uint64_t scale_out, 
+                boost::uint64_t init_out = 0, 
+                boost::uint64_t init_in = 0)
                 : scale_in_(1)
                 , scale_out_(1)
-                , last_in_(0)
-                , last_out_(0)
+                , last_in_(init_in)
+                , last_out_(init_out)
                 , last_left_(0)
             {
-                reset(scale_in, scale_out);
+                calc_scale(scale_in, scale_out);
             }
- 
+
         public:
             static boost::uint64_t static_transfer(
                 boost::uint64_t scale_in, 
                 boost::uint64_t scale_out, 
-                boost::uint64_t n)
+                boost::uint64_t n, 
+                boost::uint64_t * l = NULL)
             {
                 boost::uint64_t out = scale_out / scale_in;
                 boost::uint64_t left = scale_out - out * scale_in;
@@ -69,72 +72,79 @@ namespace framework
                         left -= scale_in;
                     }
                 }
+                if (l) {
+                    *l = left2;
+                }
                 return out2;
             }
 
-            boost::uint64_t static_transfer(
-                boost::uint64_t n) const
+            // 与直接反向转换不同的是，它保留了转换双方之间的偏移量
+            boost::uint64_t revert(
+                boost::uint64_t n, 
+                boost::uint64_t * l = NULL) const
             {
                 if (scale_in_ == 1) {
                     if (scale_out_ == 1) {
                         return n;
                     } else {
-                        return n * scale_out_;
+                        return n / scale_out_;
                     }
                 }
-                boost::uint64_t out = trans1_[n & 0xff].out;
-                boost::uint64_t left = trans1_[n & 0xff].left;
-                n >>= 8;
-                if (n) {
-                    size_t i = 0;
-                    do {
-                        if (n & 1) {
-                            out += trans2_[i].out;
-                            left += trans2_[i].left;
-                            if (left >= scale_in_) {
-                                ++out;
-                                left -= scale_in_;
-                            }
-                        }
-                        n >>= 1;
-                        ++i;
-                    } while (n);
+                boost::uint64_t scale_in = scale_out_;
+                boost::uint64_t scale_out = scale_in_;
+                boost::uint64_t in = last_out_;
+                boost::uint64_t out = last_in_;
+                boost::uint64_t left = last_left_;
+                if (left) {
+                    --out;
+                    left = scale_in - left;
+                }
+                boost::uint64_t left2 = 0;
+                if ((boost::int64_t)n > (boost::int64_t)in) {
+                    out += static_transfer(scale_in, scale_out, n - in, &left2);
+                    left += left2;
+                    if (left >= scale_in) {
+                        ++out;
+                        left -= scale_in;
+                    }
+                } else if ((boost::int64_t)n < (boost::int64_t)in) {
+                    out -= static_transfer(scale_in, scale_out, in - n, &left2);
+                    if (left < left2) {
+                        --out;
+                        left = scale_in + left - left2;
+                    } else {
+                        left -= left2;
+                    }
+                }
+                if (l) {
+                    *l = left * common_divisor_;
                 }
                 return out;
             }
 
         public:
             void reset(
-                boost::uint64_t scale_in, 
-                boost::uint64_t scale_out)
+                boost::uint64_t out = 0, 
+                boost::uint64_t in = 0)
             {
-                common_divisor_ = common_divisor(scale_in, scale_out);
-                scale_in_ = scale_in / common_divisor_;
-                scale_out_ = scale_out / common_divisor_;
-                Transform t = {0, 0};
-                trans1_.resize(256);
-                for (size_t i = 0; i < 256; ++i) {
-                    trans1_[i] = t;
-                    t.left += scale_out_;
-                    if (t.left >= scale_in_) {
-                        boost::uint64_t n = t.left / scale_in_;
-                        t.out += n;
-                        t.left -= n * scale_in_;
-                    }
-                }
-                trans2_.resize(56);
-                for (size_t i = 0; i < 56; ++i) {
-                    trans2_[i] = t;
-                    t.out <<= 1;
-                    t.left <<= 1;
-                    if (t.left >= scale_in_) {
-                        ++t.out;
-                        t.left -= scale_in_;
-                    }
-                }
-                last_in_ = last_out_ = last_left_ = 0;
+                last_in_ = in;
+                last_out_ = out;
+                last_left_ = 0;
             }
 
+            void reset_scale(
+                boost::uint64_t scale_in, 
+                boost::uint64_t scale_out, 
+                boost::uint64_t init_out = 0, 
+                boost::uint64_t init_in = 0)
+            {
+                calc_scale(scale_in, scale_out);
+                last_in_ = init_in;
+                last_out_ = init_out;
+                last_left_ = 0;
+            }
+
+        public:
             boost::uint64_t inc()
             {
                 ++last_in_;
@@ -226,35 +236,196 @@ namespace framework
             boost::uint64_t transfer(
                 boost::uint64_t n)
             {
-                return (n > last_in_) ? inc(n - last_in_) : dec(last_in_ - n);
+                return ((boost::int64_t)n > (boost::int64_t)last_in_) 
+                    ? inc(n - last_in_) 
+                    : dec(last_in_ - n);
             }
 
-            boost::uint64_t set(
+        public:
+            // 如果不希望记录本次转换的输入输出，则使用下列 const 版本接口
+
+            boost::uint64_t inc(
+                boost::uint64_t * l = NULL) const
+            {
+                boost::uint64_t out = last_out_ + trans1_[1].out;
+                boost::uint64_t left = last_left_ + trans1_[1].left;
+                if (left >= scale_in_) {
+                    ++out;
+                }
+                if (l) {
+                    *l = left;
+                }
+                return out;
+            }
+
+            boost::uint64_t inc(
+                boost::uint64_t n, 
+                boost::uint64_t * l = NULL) const
+            {
+                if (scale_in_ == 1) {
+                    if (scale_out_ == 1) {
+                        return last_out_ + n;
+                    } else {
+                        return last_out_ + n * scale_out_;
+                    }
+                }
+                boost::uint64_t out = last_out_ + trans1_[n & 0xff].out;
+                boost::uint64_t left = last_left_ + trans1_[n & 0xff].left;
+                if (left >= scale_in_) {
+                    ++out;
+                    left -= scale_in_;
+                }
+                n >>= 8;
+                if (n) {
+                    size_t i = 0;
+                    do {
+                        if (n & 1) {
+                            out += trans2_[i].out;
+                            left += trans2_[i].left;
+                            if (left >= scale_in_) {
+                                ++out;
+                                left -= scale_in_;
+                            }
+                        }
+                        n >>= 1;
+                        ++i;
+                    } while (n);
+                }
+                if (l) {
+                    *l = left;
+                }
+                return out;
+            }
+
+            boost::uint64_t dec(
+                boost::uint64_t n, 
+                boost::uint64_t * l = NULL) const
+            {
+                if (scale_in_ == 1) {
+                    if (scale_out_ == 1) {
+                        return last_out_ - n;
+                    } else {
+                        return last_out_ - n * scale_out_;
+                    }
+                }
+                boost::uint64_t out = last_out_ - trans1_[n & 0xff].out;
+                boost::uint64_t left = last_left_;
+                if (left >= trans1_[n & 0xff].left) {
+                    left -= trans1_[n & 0xff].left;
+                } else {
+                    --out;
+                    left += scale_in_ - trans1_[n & 0xff].left;
+                }
+                n >>= 8;
+                if (n) {
+                    size_t i = 0;
+                    do {
+                        if (n & 1) {
+                            out -= trans2_[i].out;
+                            if (left >= trans2_[i].left) {
+                                left -= trans2_[i].left;
+                            } else {
+                                --out;
+                                left += scale_in_ - trans2_[i].left;
+                            }
+                        }
+                        n >>= 1;
+                        ++i;
+                    } while (n);
+                }
+                if (l) {
+                    *l = left;
+                }
+                return out;
+            }
+
+            boost::uint64_t transfer(
+                boost::uint64_t n, 
+                boost::uint64_t * l = NULL) const
+            {
+                return ((boost::int64_t)n > (boost::int64_t)last_in_) 
+                    ? inc(n - last_in_, l) 
+                    : dec(last_in_ - n, l);
+            }
+
+        public:
+            // 下面两个接口单独设置 in 或者 out，实际上是要反映转换双方之间的偏移量
+
+            void last_in(
+                boost::uint64_t in)
+            {
+                last_in_ = in;
+            }
+
+            void last_out(
                 boost::uint64_t out, 
                 boost::uint64_t left = 0)
             {
-                last_in_ = 0;
                 last_out_ = out;
                 last_left_ = left;
-                return last_in_;
             }
- 
+
+        public:
             boost::uint64_t get() const
             {
                 return last_out_;
             }
- 
+
+            boost::uint64_t last_in() const
+            {
+                return last_in_;
+            }
+
+            boost::uint64_t last_out(
+                boost::uint64_t * left = NULL) const
+            {
+                if (left) {
+                    *left = last_left_ * common_divisor_;
+                }
+                return scale_out_;
+            }
+
             boost::uint64_t scale_in() const
             {
                 return scale_in_ * common_divisor_;
             }
- 
+
             boost::uint64_t scale_out() const
             {
                 return scale_out_ * common_divisor_;
             }
- 
+
         private:
+            void calc_scale(
+                boost::uint64_t scale_in, 
+                boost::uint64_t scale_out)
+            {
+                common_divisor_ = common_divisor(scale_in, scale_out);
+                scale_in_ = scale_in / common_divisor_;
+                scale_out_ = scale_out / common_divisor_;
+                Transform t = {0, 0};
+                trans1_.resize(256);
+                for (size_t i = 0; i < 256; ++i) {
+                    trans1_[i] = t;
+                    t.left += scale_out_;
+                    if (t.left >= scale_in_) {
+                        boost::uint64_t n = t.left / scale_in_;
+                        t.out += n;
+                        t.left -= n * scale_in_;
+                    }
+                }
+                trans2_.resize(56);
+                for (size_t i = 0; i < 56; ++i) {
+                    trans2_[i] = t;
+                    t.out <<= 1;
+                    t.left <<= 1;
+                    if (t.left >= scale_in_) {
+                        ++t.out;
+                        t.left -= scale_in_;
+                    }
+                }
+            }
+
             static boost::uint64_t common_divisor(
                 boost::uint64_t scale_in, 
                 boost::uint64_t scale_out)
